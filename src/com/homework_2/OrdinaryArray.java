49a7a1e1102f9b5c05bdfed978c406dbb2aeba79
MarDatsko <datskomar@gmail.com>
2019-11-30 11:43:28 +0200

    Sorting and othrer operations with array

---
+++ src/com/homework_2/OrdinaryArray.java
@@ -0,0 +1,71 @@
+package com.homework_2;
+
+public class OrdinaryArray {
+
+    public static void main(String[] args) {
+        Integer[] simpleArray = {3, 4, 1, 2, 11};
+        Integer[] hardArray = {3, 2, 3, 1, 4, 2, 8, 3};
+
+        System.out.print("Initial array : ");
+        printArray(simpleArray);
+        reversSort(simpleArray);
+        sumPositiveNumbers(simpleArray);
+        averageValue(simpleArray);
+
+        System.out.print("Initial array : ");
+        printArray(hardArray);
+        replacingDuplicateValues(hardArray);
+    }
+
+    private static void reversSort(Integer[] array) {
+        for (int i = 0; i < array.length; ++i) {
+            Integer max = array[i];
+            for (int j = i; j < array.length; ++j) {
+                if (array[j] > max) {
+                    max = array[j];
+                    array[j] = array[i];
+                }
+                array[i] = max;
+            }
+        }
+        System.out.print("\n" + "After sorted in descending order  : ");
+        printArray(array);
+    }
+
+    private static void sumPositiveNumbers(Integer[] array) {
+        int result = 0;
+        for (int i = 0; i < array.length; ++i) {
+            if ((array[i] % 2) == 0) {
+                result += array[i];
+            }
+        }
+        System.out.println("\n" + "Result of the sum all positive numbers : " + result);
+    }
+
+    private static void averageValue(Integer[] array) {
+        double result = 0.0d;
+        for (int i = 0; i < array.length; ++i) {
+            result += array[i];
+        }
+        result /= array.length;
+        System.out.println("Average value of the array : " + result);
+    }
+
+    private static void replacingDuplicateValues(Integer[] array) {
+        for (int i = 0; i < array.length; ++i) {
+            for (int j = i + 1; j < array.length; ++j) {
+                if (array[i].equals(array[j])) {
+                    array[j] = 0;
+                }
+            }
+        }
+        System.out.print("\n" + "After replacing duplicate values  : ");
+        printArray(array);
+    }
+
+    private static void printArray(Integer[] array) {
+        for (Integer x : array) {
+            System.out.print(x + " ");
+        }
+    }
+}
